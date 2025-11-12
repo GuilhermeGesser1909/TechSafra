cac42bdf1122d7731e64b09fe09adbace43ac154
AmarildoBlum1 <amarildoblum1234567@gmail.com>
2025-11-12 11:14:03 -0300

    Estoque

--- backend/TechSafraApi/src/main/java/com/api/TechSafraApi/model/ProdutoEstoqueModel.java
+++ backend/TechSafraApi/src/main/java/com/api/TechSafraApi/model/ProdutoEstoqueModel.java
@@ -1,9 +1,7 @@
 package com.api.TechSafraApi.model;
 
 import jakarta.persistence.*;
-
 import java.io.Serializable;
-import java.util.UUID;
 
 @Entity
 @Table(name = "TB_PRODUTO_ESTOQUE")
@@ -12,35 +10,63 @@ public class ProdutoEstoqueModel implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private UUID id;
+    private Long id;
 
     private String nome;
     private int quantidade;
     private String unidade;
     private double precoUnitario;
-    
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public int getQuantidade() {
-		return quantidade;
-	}
-	public void setQuantidade(int quantidade) {
-		this.quantidade = quantidade;
-	}
-	public String getUnidade() {
-		return unidade;
-	}
-	public void setUnidade(String unidade) {
-		this.unidade = unidade;
-	}
-	public double getPrecoUnitario() {
-		return precoUnitario;
-	}
-	public void setPrecoUnitario(double precoUnitario) {
-		this.precoUnitario = precoUnitario;
-	}
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "propriedade_id", nullable = false)
+    private PropriedadeModel propriedade;
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getUnidade() {
+        return unidade;
+    }
+
+    public void setUnidade(String unidade) {
+        this.unidade = unidade;
+    }
+
+    public double getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public void setPrecoUnitario(double precoUnitario) {
+        this.precoUnitario = precoUnitario;
+    }
+
+    public PropriedadeModel getPropriedade() {
+        return propriedade;
+    }
+
+    public void setPropriedade(PropriedadeModel propriedade) {
+        this.propriedade = propriedade;
+    }
 }
